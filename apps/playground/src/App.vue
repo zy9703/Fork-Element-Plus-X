4d472ecd74cce26c7771d49363dd2f653242eb6d
何嘉悦 <hejiayue520zzy@gmail.com>
2025-04-05 16:19:17 +0800

    feat: 新增 welcome 组件

--- apps/playground/src/App.vue
+++ apps/playground/src/App.vue
@@ -2,8 +2,8 @@
 import UseBubble from './components/demo-useBubble.vue'
 import UseBubbleList from './components/demo-useBubbleList.vue'
 import UseSender from './components/demo-useSender.vue'
-import UseWelcome from './components/demo-useWelcome.vue'
 import UseThought from './components/demo-useThought.vue'
+import UseWelcome from './components/demo-useWelcome.vue'
 import ScenarioSIP from './components/hooks-useXStream-scenarioSIP.vue'
 import ScenarioSSE from './components/hooks-useXStream-scenarioSSE.vue'
 import UseRecord from './components/useRecord.vue'
@@ -34,6 +34,10 @@ import UseRecord from './components/useRecord.vue'
         <UseSender />
       </el-tab-pane>
 
+      <el-tab-pane label="Welcome">
+        <UseWelcome />
+      </el-tab-pane>
+
       <el-tab-pane label="Thought">
         <UseThought />
       </el-tab-pane>
@@ -52,10 +56,6 @@ import UseRecord from './components/useRecord.vue'
 
       <el-tab-pane label="ScenarioSIP">
         <ScenarioSIP />
-      </el-tab-pane>
-
-      <el-tab-pane label="Welcome">
-        <UseWelcome />
       </el-tab-pane>
     </el-tabs>
   </div>
--- apps/playground/src/components/demo-useWelcome.vue
+++ apps/playground/src/components/demo-useWelcome.vue
@@ -1,23 +1,60 @@
 <script setup lang="ts">
-
+import { MoreFilled, Share } from '@element-plus/icons-vue'
 </script>
 
 <template>
-  <Welcome
-    variant="filled"
-    icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
-    title="欢迎使用"
-    description="这是描述信息"
-    :style="{ background: 'linear-gradient(97deg, rgba(90,196,255,0.12) 0%, rgba(174,136,255,0.12) 100%)' }"
-  >
-    <template #extra>
-      <el-button type="text">
-        关于我
-      </el-button>
-    </template>
-  </Welcome>
+  <div class="component-container">
+    <Welcome
+      variant="filled"
+      icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
+      title="欢迎使用 Element Plus X 💖"
+      description="filled -- 这是描述信息 ~"
+      :style="{ background: 'linear-gradient(97deg, rgba(90,196,255,0.12) 0%, rgba(174,136,255,0.12) 100%)' }"
+    >
+      <template #extra>
+        <el-button type="text">
+          关于我
+        </el-button>
+      </template>
+    </Welcome>
+
+    <Welcome
+      variant="borderless"
+      icon="https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp"
+      title="欢迎使用 Element Plus X 💖"
+      description="borderless  -- 这是描述信息 ~"
+      :style="{ background: 'linear-gradient(97deg, rgba(90,196,255,0.12) 0%, rgba(174,136,255,0.12) 100%)' }"
+    >
+      <template #extra>
+        <div class="extra-container">
+          <el-button plain color="#ff2828">
+            <el-icon><Share /></el-icon>
+          </el-button>
+
+          <el-button plain>
+            <el-icon><MoreFilled /></el-icon>
+          </el-button>
+        </div>
+      </template>
+    </Welcome>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.component-container {
+  background-color: white;
+  padding: 12px;
+  border-radius: 15px;
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
 
+  .extra-container {
+    :deep {
+      .el-icon {
+        font-size: 18px;
+      }
+    }
+  }
+}
 </style>
